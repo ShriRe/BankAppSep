fa278b7c8b8a51f47ce2aea868de728141844811
Shreerekha dodla <ShreerekhaK@hotmail.com>
2017-10-01 10:16:43 -0700

    First

---
+++ BankApp/Accounts.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    
+        public enum StudentType
+        {
+            Savings,
+            Current,
+            Fixed,
+            
+        }
+         /// <summary>
+        /// This is Add new  bank account application 
+        /// </summary>
+        public class Account
+        {
+            private static int LastAccountNumber = 0;
+        
+            #region Properties
+            /// <summary>
+            /// This holds the New Account number
+            /// </summary>
+            public int AccountNumber { get; }
+            /// <summary>
+            /// This holds the BAnk Account holder Email address
+            /// </summary>
+            public string EmailAddress { get; set; }
+            /// <summary>
+            /// This holds the balance
+            /// </summary>
+            public decimal Balance { get; private set; }
+            /// <summary>
+            /// This holds the account type..
+            /// </summary>
+            public StudentType AccountType { get; set; }
+            /// <summary>
+            /// This holds the new account application created date
+            /// </summary>
+            public DateTime CreatedDate { get; set; }
+            #endregion
+        /// <summary>
+        /// Constructor has no return type name of constructer same as class name
+        /// Here Constructer is looking for the memory
+        /// </summary>
+            #region constructor
+
+            public Account()
+            {
+
+             AccountNumber = ++LastAccountNumber;
+            }
+
+            #endregion
+
+            #region Methods
+        /// <summary>
+        /// Methods for Deposit and Withdraw
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+            public decimal Deposit(decimal amount)
+            {
+                Balance += amount; //take my existing account balance and add 
+                return Balance;
+            }
+            /// <summary>
+            /// This method holds for  withdraw
+            /// </summary>
+            /// <param name="criteria"></param>
+
+            public void Withdraw (decimal amount)
+            {
+            Balance -= amount;
+                
+            }
+
+            #endregion
+
+        }
+    }
+
+    
---
+++ BankApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+    {
+         {
+            #region Methods
+            
+            {
+             //Instantiate an object use Var 
+             var account = new Account();
+           //account.AccountNumber = 1234;
+             account.EmailAddress = "shreerekha.dodla16@northwestu.edu";
+             account.AccountType = StudentType.Savings;
+             // Account method
+             var newBalance = account.Deposit(100);
+             
+             //Inline FOrmatting 
+
+             Console.WriteLine($"AN : {account.AccountNumber}) ,EA: {account.EmailAddress} , BL : {account.Balance} ,AT :{account.AccountType} ");
+
+            var account2 = new Account();
+
+            Console.WriteLine($"AN : {account2.AccountNumber}) ,EA: {account.EmailAddress} , BL : {account2.Balance} ,AT :{account2.AccountType} ");
+                #endregion
+            }
+        }
+    }
+       
+        }
+    }
+
